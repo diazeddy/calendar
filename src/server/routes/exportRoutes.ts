2cfd4b17e265ec066e1f9157d5ca552c33406184
Eddy Diaz <eddydiaz@gmail.com>
2024-05-04 22:11:09 -0500

    delete the persist file after download

--- src/server/routes/exportRoutes.ts
+++ src/server/routes/exportRoutes.ts
@@ -9,17 +9,6 @@ router.get('/export/:year/:month', async (req: RequestWithMonthParams, res) => {
     try {
         const { year, month } = req.params;
         const notes = await getNotes(req, res);
-
-        const filename = `notes_${year}_${month}.json`;
-
-        fs.writeFile(filename, JSON.stringify(notes), (err) => {
-            if (err) {
-                console.error('Error exporting notes', err);
-                res.status(500).json({ error: 'Error exporting notes' });
-            } else {
-                res.download(filename);
-            }
-        });
     } catch (error) {
         console.error('Error exporting notes', error);
         res.status(500).json({ error: 'Error exporting notes' })
